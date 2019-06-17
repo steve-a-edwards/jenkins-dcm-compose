69b04d01616706fa59e1d5ad0114794d256d21aa
steve-a-edwards <steve@escala.co.uk>
2019-06-17 21:29:20 +0100

    Produced working import and export bash scripts.

---
+++ A-Jenkins-Only/jenkins/scripts/import-jobs.sh
@@ -0,0 +1,14 @@
+echo "This script will import Jenkins job XML config files"
+id="steve";
+pw="steve";
+files=`ls job-configs/*.xml`;
+echo "Processing files";
+echo $files;
+for f in $files;
+do
+  echo "File name: ${f}";
+  job_name=$(basename ${f%.*});
+  echo "Job name: ${job_name}";
+  echo "Processing file: ${f}";
+  java -jar $JENKINS_HOME/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 -auth $id:$pw create-job $job_name < $f;
+done
--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Jenkins / DCM / DataPower using Docker Compose
 
 DCM product from
 https://raw.githubusercontent.com/ibm-datapower/datapower-configuration-manager/master/deploy.ant.xml
-
 https://github.com/ibm-datapower/datapower-configuration-manager/releases/download/20/dcm.jar
